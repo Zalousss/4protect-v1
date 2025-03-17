03b83c54522f60578b545bbb92d035f5e9f1bd93
Zalousss <140243211+Zalousss@users.noreply.github.com>
2025-03-17 19:08:11 +0100

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '637036025440501780', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: 'SK', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'S', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
